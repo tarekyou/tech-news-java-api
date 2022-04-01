7900e2e4dbaefe7b3609113ddab1f2142c02d7a9
tarekyou <tarekyoussef88@hotmail.com>
2022-03-31 22:56:16 -0700

    repositories

---
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -0,0 +1,15 @@
+//package com.technews.repository;
+//
+//public interface UserRepository {
+//}
+
+package com.technews.repository;
+
+import com.technews.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByEmail(String email) throws Exception;
+}
