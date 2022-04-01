16db8c47b24ede41f1b3036ce8a30edf5148cbb1
tarekyou <tarekyoussef88@hotmail.com>
2022-03-31 23:04:05 -0700

    application properties

--- src/main/java/com/technews/repository/VoteRepository.java
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-    
+
     @Query("SELECT count(*) FROM Vote v where v.postId = :id")
     int countVotesByPostId(@Param("id") Integer id);
 
